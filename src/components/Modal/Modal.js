8c69c99062ff8d6756bb04bde717f17854d51a2e
Andre Motta <andremotta16041998@gmail.com>
2022-03-04 11:18:18 -0300

    modificação no modal
    Uma das modificações serve para limpar o modal quando o mesmo for fechado.
    
    A outra modificacao serve para controlar a "limpeza" que o handleClick faz.
    
    E a ultima é a criação de um atributo aberto para o card.

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -16,30 +16,28 @@ import { ManipulateContext } from "../../context/ManipulaItem/ManipulateItem";
 import prettier from "prettier";
 import pluginsLista from "../../utils/plugins";
 import crud from "../../utils/crud";
+import possuiAtributos from "../../utils/possuiAtributos";
 
-const Modal = ({ setModalActive }) => {
-  const { manipulableItem, addManipulableItem } = useContext(ManipulateContext);
-  const [nome, setNome] = useState(
-    manipulableItem.nome ? manipulableItem.nome : ""
-  );
-  const [desc, setDesc] = useState(
-    manipulableItem.descricao ? manipulableItem.descricao : ""
-  );
-  const [id, setId] = useState(manipulableItem.id ? manipulableItem.id : "");
-  const [language, setLanguage] = useState(
-    manipulableItem.language ? manipulableItem.language : ""
-  );
-  const [code, setCode] = useState(
-    manipulableItem.code ? manipulableItem.code : ""
-  );
-  const [label, setLabel] = useState(
-    manipulableItem.labelLanguage ? manipulableItem.labelLanguage : ""
-  );
+const Modal = ({ setModalActive, modalActive }) => {
+  const { manipulableItem, addManipulableItem, allCards } =
+    useContext(ManipulateContext);
+  const [nome, setNome] = useState("");
+  const [desc, setDesc] = useState("");
+  const [id, setId] = useState("");
+  const [language, setLanguage] = useState("");
+  const [code, setCode] = useState("");
+  const [label, setLabel] = useState("");
+  const [newItem, setNewItem] = useState({});
+  const [oldItem, setOldItem] = useState({});
+  const [mexidos, setMexidos] = useState([]);
+
+  console.log(oldItem);
+  console.log(newItem);
   // caso o container do modal seja clicado, o modal  fecha
   function handleClick(e) {
     if (e.currentTarget == e.target) {
       setModalActive(false);
-      addManipulableItem({});
+      if (!manipulableItem.aberto) addManipulableItem({});
     }
   }
   // evento de submit
@@ -59,59 +57,106 @@ const Modal = ({ setModalActive }) => {
     // caso nao exista
     if (!id) {
       obj.novo = true;
-      console.log(obj);
+      setNewItem(obj);
       addManipulableItem(obj);
-      setModalActive(false);
     }
     // caso exista
     else {
       obj.novo = false;
-      crud.atualizar(id, obj);
+      setOldItem(obj);
       addManipulableItem(obj);
-      setModalActive(false);
     }
+    setModalActive(false);
+    setMexidos([...mexidos, obj]);
   }
+
+  // faz um reset no modal
+  useEffect(() => {
+    setNome("");
+    setDesc("");
+    setId("");
+    setLanguage("");
+    setCode("");
+    setLabel("");
+  }, [modalActive]);
+
+  // atribui valores ao modal
+  useEffect(() => {
+    if (possuiAtributos(manipulableItem) >= 1) {
+      setNome(manipulableItem.nome);
+      setDesc(manipulableItem.descricao);
+      setId(manipulableItem.id);
+      setLanguage(manipulableItem.language);
+      setCode(manipulableItem.code);
+      setLabel(manipulableItem.labelLanguage);
+    }
+    return () => {
+      manipulableItem.aberto = false;
+    };
+  }, [manipulableItem]);
+
+  // caso crie um novo item, o antigo sera "fechado"
+  useEffect(() => {
+    if (possuiAtributos(manipulableItem) >= 1) {
+      newItem.aberto = true;
+      oldItem.aberto = false;
+    }
+  }, [newItem]);
+
+  useEffect(() => {
+    if (possuiAtributos(manipulableItem) >= 1) {
+      newItem.aberto = false;
+      oldItem.aberto = true;
+    }
+  }, [oldItem]);
+
+  useEffect(() => {
+    console.log(mexidos);
+  }, [mexidos]);
+
   return (
     <>
-      <ShadowContainer onClick={handleClick}>
-        <Container className="modal">
-          <Form onSubmit={handleSubmit}>
-            <ContainerMestre>
-              <Container1>
-                <InputComponente
-                  label="Nome"
-                  border="1px solid #ccc"
-                  value={nome}
-                  onChange={(e) => {
-                    setNome(e.target.value);
-                  }}
-                ></InputComponente>
-                <TextArea
-                  label="Descricao"
-                  border="1px solid #ccc"
-                  value={desc}
-                  onChange={(e) => {
-                    setDesc(e.target.value);
-                  }}
-                />
-              </Container1>
-              <Container2>
-                <Select
-                  label="Linguagens"
-                  value={language}
-                  onChange={(e) => {
-                    setLanguage(e.target.value);
-                    setLabel(e.target.selectedOptions[0].textContent);
-                  }}
-                ></Select>
-              </Container2>
-            </ContainerMestre>
-            <Button>
-              <AiOutlineCheck size={"20px"} color="#8333C8" />
-            </Button>
-          </Form>
-        </Container>
-      </ShadowContainer>
+      {modalActive && (
+        <ShadowContainer onClick={handleClick}>
+          <Container className="modal">
+            <Form onSubmit={handleSubmit}>
+              <ContainerMestre>
+                <Container1>
+                  <InputComponente
+                    label="Nome"
+                    border="1px solid #ccc"
+                    value={nome}
+                    onChange={(e) => {
+                      setNome(e.target.value);
+                    }}
+                  ></InputComponente>
+                  <TextArea
+                    label="Descricao"
+                    border="1px solid #ccc"
+                    value={desc}
+                    onChange={(e) => {
+                      setDesc(e.target.value);
+                    }}
+                  />
+                </Container1>
+                <Container2>
+                  <Select
+                    label="Linguagens"
+                    value={language}
+                    onChange={(e) => {
+                      setLanguage(e.target.value);
+                      setLabel(e.target.selectedOptions[0].textContent);
+                    }}
+                  ></Select>
+                </Container2>
+              </ContainerMestre>
+              <Button>
+                <AiOutlineCheck size={"20px"} color="#8333C8" />
+              </Button>
+            </Form>
+          </Container>
+        </ShadowContainer>
+      )}
     </>
   );
 };
