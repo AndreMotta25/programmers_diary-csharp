ef50c031ebc73011d8bfad5887b11f53a24b6a8e
Andre Motta <andremotta16041998@gmail.com>
2022-06-05 14:02:57 -0300

    fix: melhorando o nome dos elementos do header

--- programmersdiary.FrontEnd/src/components/Header/Header.js
+++ programmersdiary.FrontEnd/src/components/Header/Header.js
@@ -1,24 +1,24 @@
-import React, { useContext, useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { HeaderWrapper, Title, Save } from "./styles";
 import { AiOutlineSave } from "react-icons/ai";
-// import { ManipulateContext } from "../../context/ManipulaItem/ManipulateItem";
 import prettier from "prettier";
 import Error from "../Error/Error";
 import crud from "../../utils/crud";
-import { pluginsLista, possuiAtributos } from "../../utils/utils";
+import { pluginsLista } from "../../utils/utils";
 import { toast } from "react-toastify";
+
 /*
   o header vai ser responsavel por salvar o conteudo que está no contexto manipulado.
 */
 const Header = ({ itemManipulavel, setManipulavelItem, codigo }) => {
   const [error, setErrors] = useState({ err: false });
-  const [teste, setTeste] = useState(false);
+  const [salvar, setSalvar] = useState(false);
   const [code, setCode] = useState(codigo);
 
   useEffect(() => {
     let identificador;
     if (itemManipulavel.aberto === true && !error.err) {
-      let funcaoTest = async () => {
+      let salvar = async () => {
         if (itemManipulavel.id) {
           itemManipulavel.codigo = codigo;
           crud.atualizar(itemManipulavel.id, itemManipulavel);
@@ -45,9 +45,9 @@ const Header = ({ itemManipulavel, setManipulavelItem, codigo }) => {
           codigo: codigo,
         });
       };
-      funcaoTest();
+      salvar();
     }
-  }, [teste]);
+  }, [salvar]);
 
   // vai verificar se ha erro no codigo digitado
   useEffect(() => {
@@ -74,7 +74,7 @@ const Header = ({ itemManipulavel, setManipulavelItem, codigo }) => {
     // try {
     if (itemManipulavel.nome) {
       // gatilho para invocar o useEffect de cima(é ele quem salva no banco de dados)
-      setTeste((value) => !value);
+      setSalvar((value) => !value);
     } else {
       setErrors({ err: "Crie um card antes de começar a digitar" });
     }
