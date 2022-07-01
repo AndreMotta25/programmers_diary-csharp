f1c60eb73f6b01f7e273aab7167d45ef7cb78622
AndreMotta25 <andremotta16041998@gmail.com>
2022-07-01 17:56:48 -0300

    feat: retirando os contextos

--- programmersdiary.FrontEnd/src/App.js
+++ programmersdiary.FrontEnd/src/App.js
@@ -2,9 +2,6 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import { WrapperGlobal } from "./styles";
-import ManipulateProvider from "./context/ManipulaItem/ManipulateItem";
-import NewItemProvider from "./context/NewItem/NewItem";
-import OldItemProvider from "./context/OldItem/OldItem";
 import { ToastContainer } from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.css";
@@ -12,18 +9,12 @@ import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <>
-      <ManipulateProvider>
-        <NewItemProvider>
-          <OldItemProvider>
-            <WrapperGlobal>
-              <Routes>
-                <Route path="/" element={<Home />} />
-              </Routes>
-              <ToastContainer />
-            </WrapperGlobal>
-          </OldItemProvider>
-        </NewItemProvider>
-      </ManipulateProvider>
+      <WrapperGlobal>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <ToastContainer />
+      </WrapperGlobal>
     </>
   );
 }
