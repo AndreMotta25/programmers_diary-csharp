2be82ef9cddfc1c8ef7026662abaf236b293dd34
AndreMotta25 <andremotta16041998@gmail.com>
2022-07-02 17:12:08 -0300

    feat: excluindo arquivos desnecessarios

--- programmersdiary.FrontEnd/src/index.js
+++ programmersdiary.FrontEnd/src/index.js
@@ -1,18 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
